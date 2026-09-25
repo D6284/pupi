1d61bc0b7cf9932b33c009dc0fbbd4613ee77557
D6284 <forsacdesmond80@gmail.com>
2026-09-25 13:56:04 +0100

    Fix deployed pages and chat assets

--- pups/server.js
+++ pups/server.js
@@ -37,6 +37,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get(['/index.html', '/category/all-products.html', '/prosses.html', '/experence.html', '/policy.html', '/contact.html'], (req, res, next) => {
+  res.sendFile(path.join(__dirname, req.path), (error) => {
+    if (error) next(error);
+  });
+});
+
 // Serve public static assets
 app.use(express.static(__dirname));
 
